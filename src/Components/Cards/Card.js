4f5e0e3079b23c9451316ca9d3376c6d73a8c890
Cierra Washington <cierrajwashi@gmail.com>
2018-09-20 23:29:40 -0600

    Adjust text alignment in cards

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -15,8 +15,10 @@ export default class Card extends Component{
 render(){
   return(
     <article className="card-article">
-    <h1>{this.props.title}</h1>
-    <h1>{this.props.description}</h1>
+    <section className="info-section">
+    <h1 className="title-text">Idea:</h1><h2 className="title-value">{this.props.title}</h2>
+    <h1 className="description-text">Description:</h1><h2 className="description-value">{this.props.description}</h2>
+    </section>
     </article>
   )
 }
